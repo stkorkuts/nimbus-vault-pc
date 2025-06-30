5344927c05f92ec3773a6866b8ea102370279c56
Stanislau Korkuts <stkorkuts@gmail.com>
2025-06-30 16:24:29 +0300

    Implement initial project structure and setup

---
+++ nimbus-vault-pc-client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-black mb-4">Hello Tailwind!</h1>
+        <p className="text-white-700">
+          Your Tauri app is now styled with Tailwind CSS.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
